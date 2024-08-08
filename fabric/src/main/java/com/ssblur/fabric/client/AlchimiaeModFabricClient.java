ced57fff61228977a6139462224fc60f45c4f5c0
Patrick Hallbick <patrick.hallbick@outlook.com>
2024-08-08 15:53:19 -0400

    Update metadata

---
+++ fabric/src/main/java/com/ssblur/fabric/client/AlchimiaeModFabricClient.java
@@ -0,0 +1,8 @@
+package com.ssblur.fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public final class AlchimiaeModFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {}
+}
