554653de9a060be3a5ccaf7bb58f2d2f188de01f
Patrick Hallbick <patrick.hallbick@outlook.com>
2025-03-16 00:46:33 -0400

    Apply proper color to ThrownPotions

--- common/src/main/java/com/ssblur/alchimiae/mixin/ThrownPotionMixin.java
+++ common/src/main/java/com/ssblur/alchimiae/mixin/ThrownPotionMixin.java
@@ -1,12 +1,20 @@
 package com.ssblur.alchimiae.mixin;
 
+import com.ssblur.alchimiae.data.AlchimiaeDataComponents;
 import com.ssblur.alchimiae.item.potions.CustomThrowAction;
+import net.minecraft.core.component.DataComponentType;
 import net.minecraft.world.entity.projectile.ThrownPotion;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.PotionContents;
 import net.minecraft.world.phys.HitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.Optional;
 
 @Mixin(ThrownPotion.class)
 public class ThrownPotionMixin {
@@ -17,5 +25,19 @@ public class ThrownPotionMixin {
         if(item.getItem() instanceof CustomThrowAction action) {
             action.onHit(item, hitResult, self);
         }
+    }
+
+    @Redirect(
+        method = "onHit",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/world/item/ItemStack;getOrDefault(Lnet/minecraft/core/component/DataComponentType;Ljava/lang/Object;)Ljava/lang/Object;"
+        )
+    )
+    private Object alchimiae$onHit$getOrDefault(ItemStack instance, DataComponentType dataComponentType, Object o) {
+        var customEffects = instance.get(AlchimiaeDataComponents.INSTANCE.getCUSTOM_POTION());
+        if(customEffects != null)
+            return new PotionContents(Optional.empty(), Optional.of(customEffects.getColor()), List.of());
+        return instance.getOrDefault(dataComponentType, o);
     }
 }
