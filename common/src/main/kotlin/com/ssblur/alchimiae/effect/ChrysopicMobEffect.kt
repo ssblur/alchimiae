2c760602fb61fd62768fb8f7b50c45910554451c
Patrick Hallbick <patrick.hallbick@outlook.com>
2025-07-17 09:02:33 -0400

    Berf Chrysopic

--- common/src/main/kotlin/com/ssblur/alchimiae/effect/ChrysopicMobEffect.kt
+++ common/src/main/kotlin/com/ssblur/alchimiae/effect/ChrysopicMobEffect.kt
@@ -2,8 +2,10 @@ package com.ssblur.alchimiae.effect
 
 import net.minecraft.world.effect.MobEffect
 import net.minecraft.world.effect.MobEffectCategory
+import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.Items
 import net.minecraft.world.phys.Vec3
 
 class ChrysopicMobEffect() :
@@ -13,8 +15,9 @@ class ChrysopicMobEffect() :
   }
 
   override fun applyEffectTick(livingEntity: LivingEntity, i: Int): Boolean {
+    val modifier = i + if(livingEntity.getItemBySlot(EquipmentSlot.CHEST).`is`(Items.ELYTRA)) 3 else 0
     if(livingEntity.isFallFlying)
-      livingEntity.addDeltaMovement(Vec3(0.0, -0.005, 0.0))
+      livingEntity.addDeltaMovement(Vec3(0.0, 0.0035 * modifier - 0.007, 0.0))
     return super.applyEffectTick(livingEntity, i)
   }
 
