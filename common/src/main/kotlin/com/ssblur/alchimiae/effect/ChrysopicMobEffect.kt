cc8fbedc5e4c47ef4ef0f1cb92ef7b7674966ccc
Patrick Hallbick <patrick.hallbick@outlook.com>
2025-07-18 14:55:52 -0400

    Alter Chrysopic scaling

--- common/src/main/kotlin/com/ssblur/alchimiae/effect/ChrysopicMobEffect.kt
+++ common/src/main/kotlin/com/ssblur/alchimiae/effect/ChrysopicMobEffect.kt
@@ -6,7 +6,6 @@ import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Items
-import net.minecraft.world.phys.Vec3
 
 class ChrysopicMobEffect() :
   MobEffect(MobEffectCategory.BENEFICIAL, 0xff5533aau.toInt()) {
@@ -15,7 +14,7 @@ class ChrysopicMobEffect() :
   override fun applyEffectTick(livingEntity: LivingEntity, i: Int): Boolean {
     val modifier = i + if(livingEntity.getItemBySlot(EquipmentSlot.CHEST).`is`(Items.ELYTRA)) 3 else 0
     if(livingEntity.isFallFlying)
-      livingEntity.addDeltaMovement(Vec3(0.0, 0.0035 * modifier - 0.007, 0.0))
+      livingEntity.deltaMovement = livingEntity.deltaMovement.scale(1.0 + (0.0045 * modifier - 0.009))
     return super.applyEffectTick(livingEntity, i)
   }
 
