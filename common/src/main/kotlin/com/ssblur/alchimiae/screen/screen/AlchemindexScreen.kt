27401642b0b12f7cc74d474d6b700e2dfaa0f233
Juniper Hallbick <pat.hallbick@outlook.com>
2026-09-21 15:56:02 -0400

    Improve pagination and spacing in Alchemindex Add Back button to Alchemindex

--- common/src/main/kotlin/com/ssblur/alchimiae/screen/screen/AlchemindexScreen.kt
+++ common/src/main/kotlin/com/ssblur/alchimiae/screen/screen/AlchemindexScreen.kt
@@ -7,13 +7,12 @@ import com.ssblur.alchimiae.screen.screen.widget.ItemButtonWidget
 import com.ssblur.unfocused.extension.SoundEventExtension.play
 import com.ssblur.unfocused.screen.UnfocusedScreen
 import com.ssblur.unfocused.screen.renderable.SinglePageBackground
-import com.ssblur.unfocused.screen.widget.NextPageWidget
-import com.ssblur.unfocused.screen.widget.PlainTextWidget
-import com.ssblur.unfocused.screen.widget.PrevPageWidget
-import com.ssblur.unfocused.screen.widget.TitleWidget
+import com.ssblur.unfocused.screen.widget.*
 import net.minecraft.ChatFormatting
+import net.minecraft.client.Minecraft
 import net.minecraft.client.resources.language.I18n
 import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.Style
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.sounds.SoundEvents
 import net.minecraft.world.entity.player.Inventory
@@ -48,25 +47,32 @@ class AlchemindexScreen(abstractContainerMenu: AlchemindexMenu, inventory: Inven
     // title
     add(TitleWidget(
       Component.translatable(effect!!.toLanguageKey("effect")),
-      leftPos + 24, topPos + 24, imageWidth - 24, 64, false
+      leftPos + 24, topPos + 24, imageWidth - 48, 64, false
     ))
 
-    var y = topPadding
+    var y = TOP_PADDING
 
+    val font = Minecraft.getInstance().font
+    var captionHeight = 0
     if(I18n.exists(effect!!.toLanguageKey("effect", "description"))) {
+      captionHeight = font.splitter.splitLines(
+        I18n.get((effect!!.toLanguageKey("effect", "description"))),
+        imageWidth - 48,
+        Style.EMPTY
+      ).size * font.lineHeight
       add(
         PlainTextWidget(
           Component.translatable(effect!!.toLanguageKey("effect", "description"))
             .withStyle(ChatFormatting.GRAY),
-          leftPos + 24, topPos + 50, imageWidth - 24, 64, false
+          leftPos + 24, topPos + 50, imageWidth - 48, captionHeight, false
         )
       )
-      y += 18
+      y += captionHeight + 2
     }
 
-    var x = leftPadding
-    val w = imageWidth - rightPadding
-    val h = imageHeight - bottomPadding
+    var x = LEFT_PADDING
+    val w = imageWidth - RIGHT_PADDING
+    val h = imageHeight - BOTTOM_PADDING
     var curPage = 0
     // filter entries to items which have this effect
     val items = ClientAlchemyHelper.EFFECTS.entries.filter { (key, value) ->
@@ -76,7 +82,7 @@ class AlchemindexScreen(abstractContainerMenu: AlchemindexMenu, inventory: Inven
     }.filterNotNull()
     for(i in items) {
       if(curPage == page) {
-        add(ItemButtonWidget(leftPos + x, topPos + y, effectSize, i) {
+        add(ItemButtonWidget(leftPos + x, topPos + y, EFFECT_SIZE, i) {
           item = i
           effect = null
           page = 0
@@ -84,16 +90,19 @@ class AlchemindexScreen(abstractContainerMenu: AlchemindexMenu, inventory: Inven
           SoundEvents.UI_BUTTON_CLICK.play()
         })
       } else if(curPage > page) break
-      x += itemSize + itemSpacing
-      if((x + itemSize) > w) {
-        y += itemSize + itemSpacing
-        x = leftPadding
-        if((y + effectSize) > h) {
+      x += ITEM_SIZE + ITEM_SPACING
+      if((x + ITEM_SIZE) > w) {
+        y += ITEM_SIZE + ITEM_SPACING
+        x = LEFT_PADDING
+        if((y + EFFECT_SIZE) > h) {
           curPage++
-          y = topPadding
+          y = TOP_PADDING + captionHeight + 2
         }
       }
     }
+
+    addPageButtons(curPage)
+    addBackButton()
   }
 
   fun initItemPage() {
@@ -105,18 +114,18 @@ class AlchemindexScreen(abstractContainerMenu: AlchemindexMenu, inventory: Inven
     val effects = ClientAlchemyHelper.EFFECTS.entries.filter{ (k, _) ->
       k == item
     }.flatMap { (_, v) -> v?.toList() ?: listOf() }
-      .distinct()
+//      .distinct() // actually for items it seems useful to show total number of effects
       .sortedBy {
         I18n.get(it.toLanguageKey("effect"))
       }
-    var y = topPadding
-    var x = leftPadding
-    val w = imageWidth - rightPadding
-    val h = imageHeight - bottomPadding
+    var y = TOP_PADDING
+    var x = LEFT_PADDING
+    val w = imageWidth - RIGHT_PADDING
+    val h = imageHeight - BOTTOM_PADDING
     var curPage = 0
     for(e in effects) {
       if(curPage == page) {
-        add(EffectButtonWidget(leftPos + x, topPos + y, effectSize, e) {
+        add(EffectButtonWidget(leftPos + x, topPos + y, EFFECT_SIZE, e) {
           effect = e
           item = null
           page = 0
@@ -125,16 +134,19 @@ class AlchemindexScreen(abstractContainerMenu: AlchemindexMenu, inventory: Inven
         })
       } else if(curPage > page) break
 
-      x += effectSize + effectSpacing
-      if((x + effectSize) > w) {
-        y += effectSize + effectSpacing
-        x = leftPadding
-        if((y + effectSize) > h) {
+      x += EFFECT_SIZE + EFFECT_SPACING
+      if((x + EFFECT_SIZE) > w) {
+        y += EFFECT_SIZE + EFFECT_SPACING
+        x = LEFT_PADDING
+        if((y + EFFECT_SIZE) > h) {
           curPage++
-          y = topPadding
+          y = TOP_PADDING
         }
       }
     }
+
+    addPageButtons(curPage)
+    addBackButton()
   }
 
   fun initIndexPage() {
@@ -146,20 +158,20 @@ class AlchemindexScreen(abstractContainerMenu: AlchemindexMenu, inventory: Inven
 
     var headerDrawn = false
     var curPage = 0
-    var y = topPadding
-    var x = leftPadding
-    val w = imageWidth - rightPadding
-    val h = imageHeight - bottomPadding
+    var y = TOP_PADDING
+    var x = LEFT_PADDING
+    val w = imageWidth - RIGHT_PADDING
+    val h = imageHeight - BOTTOM_PADDING
     for(e in effects) {
       if(curPage == page) {
         if(!headerDrawn) {
           add(TitleWidget(
             Component.translatable("gui.alchimiae.effects"),
-            leftPos + 24, topPos + 24, imageWidth - 24, 64, false
+            leftPos + 24, topPos + 24, imageWidth - 48, 64, false
           ))
           headerDrawn = true
         }
-        add(EffectButtonWidget(leftPos + x, topPos + y, effectSize, e) {
+        add(EffectButtonWidget(leftPos + x, topPos + y, EFFECT_SIZE, e) {
           effect = e
           item = null
           page = 0
@@ -168,20 +180,20 @@ class AlchemindexScreen(abstractContainerMenu: AlchemindexMenu, inventory: Inven
         })
       } else if(curPage > page) break
 
-      x += effectSize + effectSpacing
-      if((x + effectSize) > w) {
-        y += effectSize + effectSpacing
-        x = leftPadding
-        if((y + effectSize) > h) {
+      x += EFFECT_SIZE + EFFECT_SPACING
+      if((x + EFFECT_SIZE) > w) {
+        y += EFFECT_SIZE + EFFECT_SPACING
+        x = LEFT_PADDING
+        if((y + EFFECT_SIZE) > h) {
           curPage++
-          y = topPadding
+          y = TOP_PADDING
         }
       }
     }
 
     curPage++
-    x = leftPadding
-    y = topPadding
+    x = LEFT_PADDING
+    y = TOP_PADDING
     val items = ClientAlchemyHelper.EFFECTS.keys.filterNotNull().filter {
       item != Items.AIR
     }
@@ -194,7 +206,7 @@ class AlchemindexScreen(abstractContainerMenu: AlchemindexMenu, inventory: Inven
           ))
           headerDrawn = true
         }
-        add(ItemButtonWidget(leftPos + x, topPos + y, effectSize, i) {
+        add(ItemButtonWidget(leftPos + x, topPos + y, EFFECT_SIZE, i) {
           item = i
           effect = null
           page = 0
@@ -202,17 +214,21 @@ class AlchemindexScreen(abstractContainerMenu: AlchemindexMenu, inventory: Inven
           SoundEvents.UI_BUTTON_CLICK.play()
         })
       } else if(curPage > page) break
-      x += itemSize
-      if((x + itemSize) > w) {
-        y += itemSize + itemSpacing
-        x = leftPadding
-        if((y + effectSize) > h) {
+      x += ITEM_SIZE
+      if((x + ITEM_SIZE) > w) {
+        y += ITEM_SIZE + ITEM_SPACING
+        x = LEFT_PADDING
+        if((y + EFFECT_SIZE) > h) {
           curPage++
-          y = topPadding
+          y = TOP_PADDING
         }
       }
     }
 
+    addPageButtons(curPage)
+  }
+
+  fun addPageButtons(curPage: Int) {
     if(curPage != page) {
       add(NextPageWidget(leftPos + imageWidth - 44, topPos + imageHeight - 35) {
         page++
@@ -228,14 +244,31 @@ class AlchemindexScreen(abstractContainerMenu: AlchemindexMenu, inventory: Inven
     }
   }
 
+  fun addBackButton() {
+    val width = 46
+    val half = width / 2
+    add(ButtonWidget(
+      leftPos + (imageWidth / 2) - half,
+      topPos + imageHeight - 50,
+      width,
+      24,
+      Component.translatable("gui.alchimiae.back")
+    ) {
+      item = null
+      effect = null
+      page = 0
+      rebuildWidgets()
+    })
+  }
+
   companion object {
-    val effectSize = 16
-    val effectSpacing = 2
-    val itemSize = 16
-    val itemSpacing = 2
-    val leftPadding = 24
-    val rightPadding = 24
-    val bottomPadding = 36
-    val topPadding = 48
+    const val EFFECT_SIZE = 16
+    const val EFFECT_SPACING = 2
+    const val ITEM_SIZE = 16
+    const val ITEM_SPACING = 4
+    const val LEFT_PADDING = 24
+    const val RIGHT_PADDING = 24
+    const val BOTTOM_PADDING = 48
+    const val TOP_PADDING = 48
   }
 }
